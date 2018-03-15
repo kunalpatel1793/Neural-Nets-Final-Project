3a2011008784f48bdb8b9a48fe78528c2df7544e
Nathan <nathanpilbrough@gmail.com>
2018-03-15 10:10:15 -0700

    Implemented multi-layer extraction and attempted model optimization

--- UtilNNDL.py
+++ UtilNNDL.py
@@ -28,6 +28,9 @@ now = datetime.now()
 import librosa
 from librosa import display
 
+from sklearn.metrics import confusion_matrix
+import itertools
+
 #Augments the dataset by creating windowed data samples 
 def create_window_data(data, labels, windows=10, window_size=512, time_last = True ):
     assert data.shape[0] == labels.shape[0] 
@@ -49,6 +52,40 @@ def plot_hist(vals,labels='Null',title='Null',xlabel='epochs'):
     plt.legend()
     plt.show()
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+ 
 '''
 Formats the data so there are no nans and pust it into testing and training batches
 Returns either arrays of all the data concatenated into four arrays: data and labels for test and train
